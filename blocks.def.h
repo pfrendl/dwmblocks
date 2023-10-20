4349bb217959d53eb5db6aa1a1e067d841f115c4
pfrendl <pfre00@gmail.com>
2023-10-20 03:28:59 +0200

    added volume, cpu usage, memory usage scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "~/.scripts/audio_volume_perc",    0,  1},
+	{"", "~/.scripts/cpu_usage_perc",       1,  0},
+	{"", "~/.scripts/mem_usage_perc",       1,  0},
+	{"", "date '+%b %d (%a) %I:%M%p'",      5,  0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
